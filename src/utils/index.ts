2c51dedbfdff36ef0169b670ead32b86ca73f177
Masonz <masonzone@gmail.com>
2017-12-22 22:33:38 +0800

    chore: remove all files in the utils directory

--- src/utils/index.ts
+++
@@ -1,6 +0,0 @@
-export const bufferToImage = (buffer: Buffer): string => {
-    let arrayBufferView = new Uint8Array(buffer);
-    let blob = new Blob([arrayBufferView], { type: "image/jpeg" });
-    let urlCreator = window.URL;
-    return urlCreator.createObjectURL(blob);
-}
