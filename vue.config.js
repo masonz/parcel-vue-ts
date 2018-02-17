624f3256c3566e4604a1bf8af684bdb236f09c5d
Masonz <masonzone@gmail.com>
2018-02-17 22:48:08 +0800

    fix: the "path" argument must be of type string

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,14 @@
-var TypeScriptAsset = require('parcel-bundler/src/assets/TypeScriptAsset.js');
+var TypeScriptAsset = require('parcel-bundler/src/assets/TypeScriptAsset.js')
 
 module.exports = {
-    // extractCSS: process.env.NODE_ENV === 'production',
-    customCompilers: {
-        ts: function (content, cb, compiler, filePath) {
-            let ts = new TypeScriptAsset(filePath, {}, {});
-            ts.contents = content;
-            ts.process().then((res) => {
-                cb(null, res.js);
-            });
-        }
+  // extractCSS: process.env.NODE_ENV === 'production',
+  customCompilers: {
+    ts: function(content, cb, compiler, filePath) {
+      let ts = new TypeScriptAsset(filePath, {}, { rootDir: filePath })
+      ts.contents = content
+      ts.process().then(res => {
+        cb(null, res.js)
+      })
     }
-};
+  }
+}
