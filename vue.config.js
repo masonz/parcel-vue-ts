f0829c383e2087b30688e7e144d4aa52ea949ae5
Masonz <masonzone@gmail.com>
2018-02-26 12:58:26 +0800

    chore: add autoprefixer

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
 var TypeScriptAsset = require('parcel-bundler/src/assets/TypeScriptAsset.js')
+var autoprefixer = require('autoprefixer')
 
 module.exports = {
   // extractCSS: process.env.NODE_ENV === 'production',
+  postcss: [autoprefixer()],
   customCompilers: {
     ts: function(content, cb, compiler, filePath) {
       let ts = new TypeScriptAsset(filePath, {}, { rootDir: filePath })
